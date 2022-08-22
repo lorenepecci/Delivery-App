83b482614dffb6c55d1b1c8a4cdd1cf7c9613860
Breno Albuquerque <brenoralbu@hotmail.com>
2022-08-22 20:59:58 -0300

    feat: Add error middleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('express-async-errors');
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
 app.use(require('./routes'));
+
+app.use(errorMiddleware);
 
 module.exports = app;
