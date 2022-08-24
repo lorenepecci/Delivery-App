5bee95f6442418570b6ac6af0db2fb9dcc8fb78f
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-08-24 09:44:54 -0300

    feat: add route /register

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 // const { login } = require('../controllers/login.controller');
+const registerController = require('../controllers/register.controller');
 
 const routers = express.Router();
 
 // routers.get('/login', login);
+routers.use('/register', registerController);
 
 module.exports = routers;
