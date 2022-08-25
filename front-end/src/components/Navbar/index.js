932793a153930af7947b7872ac85ff81fe69d731
rsz <rhenansz@hotmail.com>
2022-08-25 14:55:12 -0300

    feat: added push on buttons

--- front-end/src/components/Navbar/index.js
+++ front-end/src/components/Navbar/index.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './style.css';
 
 export default function Navbar() {
+  const history = useHistory();
+
   const user = JSON.parse(localStorage.getItem('user'));
 
   const { name } = user;
 
   return (
     <nav className="products-navbar">
-      <button type="button">PRODUTOS</button>
-      <button type="button">MEUS PEDIDOS</button>
+      <button
+        type="button"
+        onClick={ history.push('/customer/products') }
+      >
+        PRODUTOS
+      </button>
+      <button
+        type="button"
+        onClick={ history.push('/customer/orders') }
+      >
+        MEUS PEDIDOS
+      </button>
       <div>
         <p>{ name }</p>
       </div>
-      <button type="button">Sair</button>
+      <button
+        type="button"
+        onClick={ localStorage.removeItem('user') }
+      >
+        Sair
+      </button>
     </nav>
   );
 }
