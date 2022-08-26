1177fd7d723734993d32573456745001273c4ff7
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-08-26 15:56:26 -0300

    feat: add `authValidation`

---
+++ back-end/src/api/middlewares/authValidation.js
@@ -0,0 +1,22 @@
+const HttpException = require('../utils/HttpException');
+const { authenticateToken } = require('../utils/JWTToken');
+
+const authenticationMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    throw new HttpException(401, 'Token not found');
+  }
+
+  const payload = await authenticateToken(token);
+
+  if (req.originalUrl.includes('admin') && payload.role !== 'administrator') {
+    throw new HttpException(401, 'Permission denied');
+  }
+
+  res.locals.payload = payload;
+
+  next();
+};
+
+module.exports = authenticationMiddleware;
