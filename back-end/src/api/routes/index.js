b41278434e17af703b450d5b0af4e567b536da4e
Breno Albuquerque <brenoralbu@hotmail.com>
2022-08-27 10:04:52 -0300

    feat: Add route post "/sales"

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const registerController = require('../controllers/register.controller');
 const loginController = require('../controllers/login.controller');
 const productsController = require('../controllers/products.controller');
+const saleController = require('../controllers/sales.controller');
 const adminController = require('../controllers/admin.controller');
 
 const routers = express.Router();
@@ -10,5 +11,6 @@ routers.use('/register', registerController);
 routers.use('/login', loginController);
 routers.get('/products', productsController.findAll);
 routers.use('/admin/manage', adminController);
+routers.use('/sales', saleController);
 
 module.exports = routers;
