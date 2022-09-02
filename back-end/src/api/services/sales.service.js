7aca52efbcd71b674cd038dd6f7ed5852e7b7f6b
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-09-02 15:10:17 -0300

    feat: add include at query

--- back-end/src/api/services/sales.service.js
+++ back-end/src/api/services/sales.service.js
@@ -1,5 +1,5 @@
 const { Sale, sequelize } = require('../../database/models');
-const { SalesProduct } = require('../../database/models');
+const { SalesProduct, User} = require('../../database/models');
 
 const create = async (saleData, userId) => {
   let createdSale;
@@ -35,7 +35,12 @@ const getByUser = async (userId) => {
 };
 
 const getById = async (id) => {
-  const sale = await Sale.findOne({ where: { id } });
+  const sale = await Sale.findOne({
+      include: [
+        { model: User, as: 'seller', attributes: { exclude: ['id', 'email', 'password', 'role'] } },
+      ],
+      where: { id },
+    });
 
   if (!sale) {
     throw new Error('Sale not found');
