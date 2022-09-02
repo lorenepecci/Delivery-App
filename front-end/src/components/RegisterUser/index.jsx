beee28b950ef24a04719e186de99fc6c0f6badf5
Breno Albuquerque <brenoralbu@hotmail.com>
2022-09-02 11:27:21 -0300

    feat: Add missing proptypes

--- front-end/src/components/RegisterUser/index.jsx
+++ front-end/src/components/RegisterUser/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import './RegisterUser.css';
 
 export default function RegisterUser({ handleRegisterClick, error }) {
@@ -106,3 +107,8 @@ export default function RegisterUser({ handleRegisterClick, error }) {
     </div>
   );
 }
+
+RegisterUser.propTypes = {
+  handleRegisterClick: PropTypes.func.isRequired,
+  error: PropTypes.bool.isRequired,
+};
