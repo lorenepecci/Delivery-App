f46171419cfceb518bc597d11352469098b8aa4d
Breno Albuquerque <brenoralbu@hotmail.com>
2022-09-02 10:57:56 -0300

    feat: Add controller to route /users/remoce/:id

--- back-end/src/api/controllers/user.controller.js
+++ back-end/src/api/controllers/user.controller.js
@@ -14,4 +14,10 @@ userRouter.get('/all', authValidation, async (_req, res) => {
   return res.status(200).json(users);
 });
 
+userRouter.delete('/remove/:id', authValidation, async (req, res) => {
+  const { id } = req.params;
+  const users = await userService.deleteUser(id);
+  return res.status(200).json(users);
+});
+
 module.exports = userRouter;
